e7182b0b56391da390e27c6c97f61a08a92a7073
francescolucco <francescolucco@googlemail.com>
2021-12-10 16:05:16 +0100

    milestone 3

--- bundle-02/main.js
+++ bundle-02/main.js
@@ -11,57 +11,78 @@
 
 
 // ESERCIZIO 1
+const myAge = 32;
+let message = '';
 function checkAge() {
-    const myAge = 32;
-    const message = '';
 
     if (myAge < 18) {
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
-        message = 'Hai più di 18 anni!';
+        message = 'Sei maggiorenne!';
     }
 }
 checkAge();
+console.log(message);
+
+// 1- E' una funzione che si accerta che l'utente abbia un età uguale o maggiore di 18 anni.
+// 2- Non sono presenti errori di sintassi
+// 3- Le due variabili myAge e message vanno dichiarate fuori dalla funzione. Inoltre non viene affrontato il caso in cui myAge sia uguale a 18 per cui il messaggio 'hai più di 18 anni!' risulta errato
+/*******************************************************************************/
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
 
-
-// ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
-
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+// // 1- è una funzione che mi restituisce il numero di colori presenti all'interno dell'array "colors"
+// // 2- lenght è errato: si scrive length
+// // 3- Non sono presetni errori di logica
+// /*******************************************************************************/
 
 
-// ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+// // ESERCIZIO 3
+// function addNumbers() {
+//     const userNumber = prompt('Inserisci un numero');
+//     const total = parseInt(userNumber)+ 12;
 
-    let grantAccess = 'false';
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers();
 
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+// // 1- E' una funzione che somma al numero richiesto dall'utente, il numero 12 e stampa a console il risultato
+// // 2- Non sono presenti errori di sintassi
+// // 3- Sì, concatena una stringa invece che sommare due valori numerici. Va aggiunto il parseInt
+// /*******************************************************************************/
 
 
-// ESERCIZIO 5 (suggerimento: c'è un solo errore)
+// // ESERCIZIO 4
+// function checkAccess() {
+//     let grantAccess = false;
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = true;
+//     }
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
+
+// // 1- E' una funzione che controlla che l'indirizzo mail inserito dall'utente sia contenuto nell'array "addresses". 
+// // 2- Sì, i valori booleani vanno senza virgolette
+// // 3- No
+// /*******************************************************************************/
+
+
+// // ESERCIZIO 5 (suggerimento: c'è un solo errore)
 function checkAccessImproved() {
     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
 
@@ -72,15 +93,16 @@ function checkAccessImproved() {
     for (let i = 0; i < addresses.length; i++) {
      const email = addresses[i];
         
-      if (userEmail.length > 5) {
+      if (addresses.length < 5) {
             
         if (email === userEmail) {
-        grantAccess = 'true';            
+        grantAccess = true;            
             
       }
         
     }
 
+}
     if (grantAccess) {
         console.log('Accesso consentito!');
     } else {
@@ -89,31 +111,8 @@ function checkAccessImproved() {
 }
 checkAccessImproved();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// // 1- E' una funzione che controlla che l'indirizzo mail inserito dall'utente sia contenuto nell'array "addresses". 
+// // 2- Sì
+// // 3- l'ultimo IF va spostato fuori dal ciclo for
+// /*******************************************************************************/
 
--- bundle-03/main.js
+++ bundle-03/main.js
@@ -25,7 +25,7 @@ const cars = [
     {
         manufacturer: 'Volkswagen',
         model: 'Golf',
-        type: 'Benzina'
+        type: 'benzina'
     },
     {
         manufacturer: 'Fiat',
@@ -50,29 +50,25 @@ const cars = [
     {
         manufacturer: 'Ford',
         model: 'Kuga',
-        type: 'Diesel'
+        type: 'diesel'
     },
     {
         manufacturer: 'Seat',
         model: 'Ibiza',
         type: 'metano'
-    }
+    },
     {
         manufacturer: 'Audi',
         model: 'R8',
-        type: 'Benzina'
+        type: 'benzina'
     },
 ];
 
-const gasolineCars = cars.filter( (auto) >= auto.type === 'benzina');
+const gasolineCars = cars.filter( (auto) => auto.type === 'benzina');
 
-const dieselCars = cars.filter( (auto) => {
-    auto.type === 'diesel';
-});
+const dieselCars = cars.filter( (auto) => auto.type === 'diesel');
 
-const otherCars = cars.filter( (auto) => {
-    return auto.type !== 'benzina' || auto.type !== 'diesel';
-});
+const otherCars = cars.filter( (auto) => auto.type !== 'benzina' && auto.type !== 'diesel');
 
 console.log('Auto a benzina');
 console.log('*******************************');
@@ -84,3 +80,11 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+
+// Per questo esercizio, rispondi a queste domande:
+// 1- Dato un array contenente i dati relativi ad un catalogo di auto, vengono filtrati dal ciclo fiter() e vengono restituiti tre diversi array contenenti i dati delle auto divisi per tipologia di carburante.
+// 2- Sì, delle lettere maiuscole sui dati del carburanti, alcune virgole che dividono i diversi oggetti, alcune parentesi nella sintassi del primo filter.
+// 3- Sì, è sbagliato utilizzare l'operatore "o" (||) nel terzo filter.
+// ​
+// *******************************************************************************/
